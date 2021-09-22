bf4f8bffba0d1b930388d50e8dcd9e4fee599f33
Sirso <lucazzz123@gmail.com>
2021-09-22 11:13:30 -0300

    Consulta simples com query methods

--- src/main/java/com/lucassoares/workshopmongo/resources/PostResource.java
+++ src/main/java/com/lucassoares/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.lucassoares.workshopmongo.resources;
 import com.lucassoares.workshopmongo.domain.Post;
 import com.lucassoares.workshopmongo.domain.User;
 import com.lucassoares.workshopmongo.dto.UserDTO;
+import com.lucassoares.workshopmongo.resources.util.URL;
 import com.lucassoares.workshopmongo.services.PostService;
 import com.lucassoares.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +28,10 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 }
